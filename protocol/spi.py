3a7209a2ad9ede2826aa822237384b6d9d1cbb31
Brycen <wershingbrycen@gmail.com>
2017-11-25 21:42:57 -0500

    Added prototype for spi analysis

---
+++ protocol/spi.py
@@ -0,0 +1,14 @@
+
+
+
+def spiAnalysis(pathToInput, pathToOutput, clockPolarity, clockChan, csChan, mosiChan, misoChan):
+    print pathToInput
+    print pathToOutput
+    print clockPolarity
+    print clockChan
+    print csChan
+    print mosiChan
+    print misoChan
+
+
+spiAnalysis("input.raw", "output.txt", True, 0, 1, 4, 5)
