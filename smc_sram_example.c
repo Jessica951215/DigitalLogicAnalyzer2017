56add4287be0a16ed94be999fe0dafc348a5f558
Christopher Hung <christopherhung96@gmail.com>
2017-12-05 17:07:59 -0500

    Add files via upload

--- smc_sram_example.c
+++ smc_sram_example.c
@@ -126,50 +126,37 @@ static void configure_console(void)
 //! [dmac_define_buffer]
 /** DMA buffer size. */
 #define DMA_BUF_SIZE    8192
-//int num_samples_to_sample = 65535;
-int num_samples_to_sample = 8192;
-//int num_samples_to_sample = 8191;
+int num_samples_to_sample = 1024;
+volatile uint32_t* waiting_buffers[2];
+dma_transfer_descriptor_t descriptor_pointers[128];
 
 
 /** DMA buffer. */
 uint32_t* g_dma_buf = (uint32_t*)SRAM_BASE_ADDRESS;
-uint16_t* g_sampledpins_buf = (uint32_t*)SRAM_BASE_ADDRESS;
+uint16_t* g_sampledpins_buf = (uint16_t*)SRAM_BASE_ADDRESS;
 //uint32_t g_dma_buf[DMA_BUF_SIZE];
-uint32_t transitionData[DMA_BUF_SIZE];
+uint8_t* transitionData = ((uint32_t*)SRAM_BASE_ADDRESS_2ND);
 //! [dmac_define_buffer]
 
 /* DMA transfer done flag. */
 volatile uint32_t g_xfer_done = 0;
 
+volatile int32_t num_times_in_handler;
+
+volatile uint32_t buffer_number;
+
+volatile uint16_t trigger_values[16];
+
+volatile int output_voltage;
+
+uint32_t transition_index;
+
 int state = 0;
 
-void start_sampling(){
-	uint32_t i;
-	dma_transfer_descriptor_t desc;
-	desc.ul_source_addr = (uint32_t) 0x400E0E00 + 0x003C;
-	desc.ul_destination_addr = (uint32_t) g_dma_buf;
-	
-	desc.ul_ctrlA = DMAC_CTRLA_BTSIZE(num_samples_to_sample) |
-	DMAC_CTRLA_SRC_WIDTH_WORD |
-	DMAC_CTRLA_DST_WIDTH_WORD;
-	
-	desc.ul_ctrlB = DMAC_CTRLB_SRC_DSCR_FETCH_DISABLE |
-		DMAC_CTRLB_DST_DSCR_FETCH_DISABLE |
-		DMAC_CTRLB_FC_MEM2MEM_DMA_FC |
-		DMAC_CTRLB_SRC_INCR_FIXED |
-		DMAC_CTRLB_DST_INCR_INCREMENTING;
-	
-	desc.ul_descriptor_addr = 0;
-	dmac_channel_single_buf_transfer_init(DMAC, DMA_CH, &desc);
-	
-	dmac_channel_enable(DMAC, DMA_CH);
-	
-	while (!dmac_channel_is_transfer_done(DMAC, DMA_CH)) {
-	}
-	state = 1;
-}
+int swapped;
 
-uint16_t get_16_pin_inputs(uint32_t sampled_int){
+
+inline uint16_t get_16_pin_inputs(uint32_t sampled_int){
 	uint16_t first4 = 0b1111 & (sampled_int >> 3);
 	uint16_t second7 = 0b11111110000 & (sampled_int >> 7);
 	uint16_t third2 = 0b1100000000000 & (sampled_int >> 10);
@@ -177,6 +164,116 @@ uint16_t get_16_pin_inputs(uint32_t sampled_int){
 	uint16_t last = 0b1000000000000000 & (sampled_int >> 14);
 	return first4|second7|third2|fourth2|last;
 }
+
+dma_transfer_descriptor_t desc1,desc2;
+
+void start_sampling(){
+	
+	
+	// DMA config
+	uint32_t cfg;
+	pmc_enable_periph_clk(ID_DMAC);
+	dmac_init(DMAC);
+	dmac_set_priority_mode(DMAC, DMAC_PRIORITY_ROUND_ROBIN);
+	dmac_enable(DMAC);
+	cfg =	DMAC_CFG_SOD_DISABLE |     /** Disable stop on done */
+	DMAC_CFG_AHB_PROT(1) |     /** Set AHB Protection */
+	DMAC_CFG_FIFOCFG_ALAP_CFG; /** FIFO Configuration */
+	dmac_channel_set_configuration(DMAC, DMA_CH, cfg);
+	uint32_t i;
+	
+	uint32_t ul_ctrlA = DMAC_CTRLA_BTSIZE(num_samples_to_sample) |
+		DMAC_CTRLA_SRC_WIDTH_WORD |
+		DMAC_CTRLA_DST_WIDTH_WORD;
+	
+	uint32_t ul_ctrlB = DMAC_CTRLB_SRC_DSCR_FETCH_FROM_MEM |
+		DMAC_CTRLB_DST_DSCR_FETCH_FROM_MEM |
+		DMAC_CTRLB_FC_MEM2MEM_DMA_FC |
+		DMAC_CTRLB_SRC_INCR_FIXED |
+		DMAC_CTRLB_DST_INCR_INCREMENTING;
+	
+	
+	desc1.ul_source_addr = (uint32_t) 0x400E0E00 + 0x003C;
+	desc1.ul_destination_addr = (uint32_t) waiting_buffers[0];
+	
+	desc1.ul_ctrlA = ul_ctrlA;
+	
+	desc1.ul_ctrlB = ul_ctrlB;
+	
+	desc1.ul_descriptor_addr = &desc2;
+		
+		
+	desc2.ul_source_addr = (uint32_t) 0x400E0E00 + 0x003C;
+	desc2.ul_destination_addr = (uint32_t) waiting_buffers[1];
+	
+	desc2.ul_ctrlA = ul_ctrlA;
+		
+	desc2.ul_ctrlB = ul_ctrlB;
+	
+	desc2.ul_descriptor_addr = &desc1;
+		
+	
+	ul_ctrlA = DMAC_CTRLA_BTSIZE(num_samples_to_sample) |
+		DMAC_CTRLA_SRC_WIDTH_WORD |
+		DMAC_CTRLA_DST_WIDTH_WORD;
+		
+	for(i = 2; i < 128; i++){
+		descriptor_pointers[i].ul_ctrlA = ul_ctrlA;
+		descriptor_pointers[i].ul_ctrlB = ul_ctrlB;
+		descriptor_pointers[i].ul_source_addr = (uint32_t) 0x400E0E00 + 0x003C;
+		descriptor_pointers[i].ul_destination_addr = (uint32_t) (g_dma_buf + i * num_samples_to_sample);
+		descriptor_pointers[i].ul_descriptor_addr = descriptor_pointers + i + 1;
+	}
+	//set last descriptor to point to null to signal done
+	descriptor_pointers[127].ul_descriptor_addr = 0;
+		
+	dmac_channel_multi_buf_transfer_init(DMAC, DMA_CH, &desc1);
+	NVIC_EnableIRQ(DMAC_IRQn);
+	dmac_enable_interrupt(DMAC, (DMAC_EBCIER_CBTC0 << DMA_CH | DMAC_EBCIDR_BTC0  << DMA_CH));
+
+	g_xfer_done = 0;
+	dmac_channel_enable(DMAC, DMA_CH);
+	while (!g_xfer_done) {
+	}
+	uint32_t a = descriptor_pointers;
+	
+	state = 1;
+}
+
+int8_t check_trigger_behavior(uint16_t first_pins, uint16_t last_pins){
+	int i;
+	for(i = 0; i < 16; i++){
+		if(trigger_values[i] != 2){
+			if(((last_pins>>i) & 1) != trigger_values[i] || ((last_pins>>i) & 1) == ((first_pins>>i) & 1)){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+
+void DMAC_Handler(void)
+{
+	if(!swapped){
+		buffer_number = !buffer_number;
+	}
+	
+	if(swapped == 0 && check_trigger_behavior(get_16_pin_inputs(waiting_buffers[buffer_number][0]),get_16_pin_inputs(waiting_buffers[buffer_number][num_samples_to_sample-1]))){
+		dmac_channel_set_descriptor_addr(DMAC,DMA_CH,descriptor_pointers+2);
+		swapped = 1;
+	}
+	
+	
+	uint32_t dma_status;
+
+	dma_status = dmac_get_status(DMAC);
+
+	if (dma_status & (DMAC_EBCIER_CBTC0 << DMA_CH)) {
+		g_xfer_done = 1;
+	}
+}
+
 
 void print_uint16_binary(uint16_t pinValues){
 	int8_t i;
@@ -186,82 +283,97 @@ void print_uint16_binary(uint16_t pinValues){
 	return;
 }
 
+//write check_trigger_behavior function
+
 void embedded_controller(){
 	state = 0;
 	int i;
 	int done = 0;
+	int start = 0;
 	while(!done){
 		switch(state){
 			case 0:
 				//clear data
-				for(i = 0; i < num_samples_to_sample;i++){
+				for(i = 0; i < num_samples_to_sample * 128;i++){
 					g_dma_buf[i] = 0;
 				}
+				for(i = 0; i < num_samples_to_sample * 128 * 4;i++){
+					transitionData[i] = 0;
+				}
+				
+				/*
+				for(i = 0; i < 16; i++){
+					trigger_values[i] = 2;
+				}
+				trigger_values[0] = 1;
+				*/
+				
+				buffer_number = 1;
+				transition_index = 0;
+				num_times_in_handler = -1;
+				swapped = 0;
+				
+				waiting_buffers[0] = g_dma_buf;
+				waiting_buffers[1] = g_dma_buf + num_samples_to_sample;
+				
 			
 				//start collecting with DMA
 				start_sampling();
 			case 1:
-				//TODO: add compression while data is being sampled
-				state = 2;
-				break;
-			case 2:
-			
-			
-				state = 3;
-				break;
-			
-				for(i = 0; i < num_samples_to_sample;i++){
-					printf("%d ", (g_dma_buf[i]>>24) & 1);
-				}
-			
-			
-				state = 3;
-				break;
-			
-			
-				int transition_index = 0;
-				for(i = 1; i < num_samples_to_sample; i++){
-					int changed_vals = (g_dma_buf[i - 1] ^ g_dma_buf[i]); // 1 wherever there was a transition
-					int j = 0;
-					//go through each channel to check if it changed, store new value
-					for(; j < 32; j++){
-						if((changed_vals>>j) & 1){
-							//create the compressed packet using:
-							//  channel number = j;
-							//  timestamp = i;
-							//  new_value = sample_array[i]>>j;
-							transitionData[transition_index] = ((g_dma_buf[i]>>j) & 1) | j<<1 | i<<6;
-							transition_index ++;
-						}
+				//grab relevant numbers from 32 sampled pins
+				//if buffer_number == 0, everything is fine
+				//if buffer_number == 1, swap first 1024 and second 1024 first, then proceed
+				if(buffer_number){
+					for(i = 0; i < num_samples_to_sample; i++){
+						uint32_t temp = waiting_buffers[buffer_number][i];
+						waiting_buffers[1][i] = waiting_buffers[0][i];
+						waiting_buffers[0][i] = temp;
 					}
 				}
-				printf("\n");
-				for(i = 0; i < transition_index;i++){
-					printf("%d ", transitionData[i]);
+				for(i = 0; i < num_samples_to_sample*128; i++){
+					g_sampledpins_buf[i] = get_16_pin_inputs(g_dma_buf[i]);
 				}
+				
+		
+				
+			
+				int transition_index = 0;
 				/*
-				//Add compressing data step (assuming you have all data already)
-				int i = 1;
-				int transition_array = SOME_ARRAY_IN_MEMORY;
-				int sample_array = SOME_POINTER_IN_MEMORY;
-				for(; i < NUM_SAMPLES; i++){
-					int changed_vals = (sample_array[i - 1] ^ sample_array[i]); // 1 wherever there was a transition
-					int j = 0;
-					//if not equal
-					//go through each channel to check if it changed, store new value
-					for(; j < 16; j++){
-						if((changed_vals>>j) & 1){
-							//create the compressed packet using:
-							//  channel number = j;
-							//  timestamp = i;
-							//  new_value = sample_array[i]>>j;
+				for(i = 1; i < num_samples_to_sample*128 + 8192*2; i++){
+					if (g_sampledpins_buf[i] != g_sampledpins_buf[i-1]){
+						int changed_vals = (g_sampledpins_buf[i - 1] ^ g_sampledpins_buf[i]); // 1 wherever there was a transition
+						int j = 0;
+						//go through each channel to check if it changed, store new value
+						for(; j < 16; j++){
+							if((changed_vals>>j) & 1){
+								//create the compressed packet using:
+								//  channel number = j;
+								//  timestamp = i;
+								//  new_value = sample_array[i]>>j;
+								uint32_t transition_point = ((g_sampledpins_buf[i]>>j) & 1) | j<<1 | i<<6;
+								uint8_t first_byte = (uint8_t)(transition_point>>16);
+								uint8_t second_byte = (uint8_t)(transition_point>>8);
+								uint8_t third_byte = (uint8_t)(transition_point>>0);
+								transitionData[3*transition_index] = first_byte;
+								transitionData[3*transition_index+1] = second_byte;
+								transitionData[3*transition_index+2] = third_byte;
+								
+								transition_index ++;
+							}
 						}
+						
 					}
 				}
 				*/
-				state = 3;
+				
+				
+				
+				
+				state = 2;
 				break;
-			case 3:
+				
+			
+			case 2:
 			/*
 				for(i = 0; i < num_samples_to_sample; i++){
 					printf("%d ", (g_dma_buf[i]>>11) & 1);
@@ -269,26 +381,42 @@ void embedded_controller(){
 				}
 				*/
 				//grab relevant numbers from 32 sampled pins
-				for(i = 0; i < num_samples_to_sample; i++){
+				/*
+				for(i = 0; i < num_samples_to_sample*4; i++){
 					g_sampledpins_buf[i] = get_16_pin_inputs(g_dma_buf[i]);
 				}
-				/*
-				for(i = 0; i < num_samples_to_sample; i++){
-					printf("%d", ~(g_sampledpins_buf[i]>>4) & 1);
-					printf("%d", ~(g_sampledpins_buf[i]>>5) & 1);
-					printf("%d", ~(g_sampledpins_buf[i]>>6) & 1);
-					printf("%d", ~(g_sampledpins_buf[i]>>7) & 1);
-					//print_uint16_binary(g_sampledpins_buf[i]);
+				for(i = 0; i < num_samples_to_sample*5; i++){
+					//printf("%d", ~(g_sampledpins_buf[i]>>4) & 1);
+					//printf("%d", ~(g_sampledpins_buf[i]>>5) & 1);
+					//printf("%d", ~(g_sampledpins_buf[i]>>6) & 1);
+					//printf("%d", ~(g_sampledpins_buf[i]>>7) & 1);
+					print_uint16_binary(g_sampledpins_buf[i]);
 					printf("\r\n");
 				}
 				*/
 				
-				for(i = 0; i < num_samples_to_sample; i += 64){
-					char a = ~(char)g_sampledpins_buf[i];
-					char b = ~(char)g_sampledpins_buf[i]>>8;
+				//printf("transition index: %d\r\n", transition_index);
+				/*
+				for(i = 0; i < num_samples_to_sample*128; i+= 64){
+					//printf("%d ", (g_dma_buf[1023*num_samples_to_sample + i]>>8) & 1);
+					//printf("%d\t",i);
+					print_uint16_binary(g_sampledpins_buf[i]);
+					printf("\r\n");
+				}
+				*/
+				while(!check_trigger_behavior(g_sampledpins_buf[0],g_sampledpins_buf[start])){
+					start += 1;
+				}
+				
+				for(i = start; i < num_samples_to_sample*128; i+= 16){
+				//for(i = 0; i < num_samples_to_sample; i+= 64){
+					char a = ~((char)g_sampledpins_buf[i]);
+					char b = ~((char)(g_sampledpins_buf[i]>>8));
 					printf("%c%c",b,a);
 				}
 				
+				
+				printf("no more data || no more data || no more data");
 				done = 1;
 				break;
 			
@@ -386,9 +514,7 @@ int main(void)
 
 	/* SMC configuration between SRAM and SMC waveforms. */
 	configure_sram(SRAM_CHIP_SELECT);
-#ifdef SRAM_CHIP_SELECT_2ND
 	configure_sram(SRAM_CHIP_SELECT_2ND);
-#endif
 	
 	
 	// DAC stuff
@@ -414,35 +540,54 @@ int main(void)
 	dacc_write_conversion_data(DACC_BASE, (uint32_t)(DACC_MAX_DATA * 1.9 / 3.3));
 	
 	
-	// DMA config
-	uint32_t cfg;
-	pmc_enable_periph_clk(ID_DMAC);
-	dmac_init(DMAC);
-	dmac_set_priority_mode(DMAC, DMAC_PRIORITY_ROUND_ROBIN);
-	dmac_enable(DMAC);
-	cfg =	DMAC_CFG_SOD_ENABLE |        /** Enable stop on done */
-			DMAC_CFG_AHB_PROT(1) |     /** Set AHB Protection */
-			DMAC_CFG_FIFOCFG_ALAP_CFG; /** FIFO Configuration */
-	dmac_channel_set_configuration(DMAC, DMA_CH, cfg);
 	
 
-	char uc_key[1000];
+	char uc_key[200];
+	
 	
 	initialize_pins();
 	int prevValue = 0;
 	while (1) {
-			
-		int numchars = 0;
-		uc_key[0] = ' ';
+		
+		int i;
+		for(i = 0; i < 16; i++){
+			trigger_values[i] = 2;
+		}
+		for(i = 0; i < 200;i++){
+			uc_key[i] = 0;
+		}
+		volatile int numchars = 0;
+		uc_key[0] = '0';
 		while(numchars == 0 || uc_key[numchars-1] != ';'){
 			usart_serial_getchar((Usart *)CONSOLE_UART, uc_key + numchars);
-			numchars ++;
+			//printf("%c",uc_key[numchars]);
+			if(uc_key[numchars] != 0){
+				numchars ++;
+			}
 		}
 		uc_key[numchars] = '\0';
-		printf("%s",uc_key);
-		printf("no more data || no more data || no more data");
-		//wait_for_button();
-		//embedded_controller();
+		
+		i = 0;
+		output_voltage = atoi(uc_key);
+		while(uc_key[i] != '\n'){
+			i++;
+		}
+		i++;
+		int j;
+		for(j = 0; j < 16; j++){
+			trigger_values[j] = atoi(uc_key+i);
+			while(uc_key[i] != ',' && uc_key[i] != ';'){
+				i++;
+			}
+			i++;
+		}
+		//printf("%s",uc_key);
+		//printf("no more data || no more data || no more data");
+		//while(1);
+		ioport_set_pin_level(LED_0_PIN, LED_0_ACTIVE);
+		
+		embedded_controller();
+		ioport_set_pin_level(LED_0_PIN, !LED_0_ACTIVE);
 	}
 	
 
